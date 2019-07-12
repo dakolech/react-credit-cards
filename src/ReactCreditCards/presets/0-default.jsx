0c51000660dc4db950ea7acf0356fefa64d37fac
Daniel Koza <daniel.koza@uxpin.com>
2019-07-12 13:19:49 +0200

    add preset

---
+++ src/ReactCreditCards/presets/0-default.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactCreditCards from '../ReactCreditCards';
+
+export default (
+  <ReactCreditCards
+    number=""
+    name=""
+    expiry=""
+    cvc=""
+    focused=""
+  />
+);
